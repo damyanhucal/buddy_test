3aad12bcb767ffae07fe602e44b8799844bf0654
deadgoatmeat <damyanhucal@gmail.com>
2019-05-03 16:53:33 -0400

    yo i changed the yo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              Yo what's good b
+              yoooooooooooo jeff set HUUUUUUUUUUUUUGE eeeeelaioy
             </Text>
           </View>
 
